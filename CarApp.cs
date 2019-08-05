b6a9891b3d24ca8c07a263b0588ffa0b35c0e45d
Flaka Mahmuti <flakamahmuti@gmail.com>
2019-08-05 18:41:58 -0400

    firstCommit

---
+++ CarApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bonus_16
+{
+    class CarApp
+    {
+        public string CarMake;
+        public string CarModel;
+        public int CarYear;
+        public double CarPrice;
+
+        public CarApp(int year, string make, string model, double price)
+        {
+            CarMake = make;
+            CarModel = model;
+            CarPrice = price;
+            CarYear = year;
+        }
+
+        public CarApp()
+        {
+        }
+
+        public void SetCarModel(string _model)
+        {
+            CarModel= _model;
+        }
+        public void SetCarMake(string _make)
+        {
+            CarMake= _make;
+        }
+        public void SetCarYear(int _year)
+        {
+            CarYear = _year;
+        }
+        public void SetCarPrice(double _price)
+        {
+            CarPrice = _price;
+        }
+         
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Bonus_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Car c = new Car();
+           Console.WriteLine(c.Make);
+           Console.WriteLine(c.Model);
+           Console.WriteLine(c.Price);
+           Console.WriteLine(c.Year);
+            List <CarApp> NewCar = new List<CarApp>();
+            Console.WriteLine("How many cars do you want to add?");
+            int i = int.Parse(Console.ReadLine());
+            for (int j = 0; j < i; j++)
+            {
+                CarApp user1 = new CarApp();
+                Console.WriteLine("Enter a car model");
+                string input1 = Console.ReadLine();
+                user1.SetCarModel(input1);
+                Console.WriteLine("Enter a car maker");
+                string input2 = Console.ReadLine();
+                user1.SetCarMake(input2);
+                Console.WriteLine("Enter car price");
+                double input3 = double.Parse(Console.ReadLine());
+                user1.SetCarPrice(input3);
+                Console.WriteLine("Enter car year");
+                int input4 = int.Parse(Console.ReadLine());
+                user1.SetCarYear(input4);
+                NewCar.Add(new CarApp(input4, input2,input1, input3));
+                Console.WriteLine();                  
+            }
+            for (int k = 0; k < NewCar.Count; k++)
+            { int t= k + 1;
+                CarApp nn = NewCar[k];
+                Console.WriteLine("Car "+t+ " Maker: " + nn.CarMake+" Model: "+nn.CarModel+" Price: "+nn.CarPrice.ToString("C2", CultureInfo.CurrentCulture) + " Year "+nn.CarYear+" ");
+            }
+            
+        }
+        
+    }
+}
